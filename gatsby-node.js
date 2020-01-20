d54a0a4bffc52338754e3cb388d7787387a507b4
Royce Townsend <git@roycetownsend.com>
2020-01-20 08:06:58 +0800

    One folder of images on the home page.

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,24 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const fs = require("fs")
+const path = require("path")
+const mkdirp = require("mkdirp")
 
-// You can delete this file if you're not using it
+// Ensure that content directory exist at the site-level
+exports.onPreBootstrap = ({ reporter, store }, themeOptions) => {
+  const { program } = store.getState()
+  const galleryPath = themeOptions.galleryPath || "content/gallery"
+  const dir = path.join(program.directory, galleryPath)
+  if (!fs.existsSync(dir)) {
+    reporter.info(`Creating the "${dir}" directory`)
+    mkdirp.sync(dir)
+  }
+}
+
+exports.createPages = ({ actions }, themeOptions) => {
+  const { createPage } = actions
+  const { basePath } = themeOptions
+
+  createPage({
+    path: basePath || "/",
+    component: path.resolve(`${__dirname}/src/pages/index.tsx`),
+  })
+}
